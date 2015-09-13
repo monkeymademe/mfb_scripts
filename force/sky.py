bf6e1bb74edf86166d0ad3cee9df7570526fa0d2
monkeymademe <raspberryjamberlin@gmail.com>
2015-09-13 22:23:04 +0000

    Updated to run dot3.py on host system and also added arguments

--- force/sky.py
+++ force/sky.py
@@ -10,9 +10,9 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 def send_to_dot3(force):
 	if force == "Light":
-		cmd_to_execute = 'sudo python /home/pi/Pimoroni/dot3k/dothat/basic/backlight.py'
+		cmd_to_execute = 'sudo python /home/pi/mfb_scripts/force/dot3.py Light'
 	if force == "Dark":
-		cmd_to_execute = 'sudo python /home/pi/Pimoroni/dot3k/dothat/basic/backlight.py'
+		cmd_to_execute = 'sudo python /home/pi/mfb_scripts/force/dot3.py Dark'
 	ssh.connect("192.168.2.51", username="pi", password="raspberry")
 	ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
 	
